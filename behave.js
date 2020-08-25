8457093faf5f368a5084a3f4eac0f37c4d1c955c
sabrina <hoekehouse@gmail.com>
2020-08-25 15:54:18 -0700

    footer background image adjust for transparency

--- behave.js
+++ behave.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
 
     $(".hb-box").click(function(e) {
         $(".hb-box").addClass("hb-out");
+        $(".dark-bg").fadeIn("slow");
         $(".modal-menu").slideDown("slow").css("display", "flex");
     });
 
     $(".menu-closeup").click(function(e){
         $(".hb-box").addClass("hb-in");
+        $(".dark-bg").fadeOut("slow");
         $(".modal-menu").slideUp("slow");
     });
 
