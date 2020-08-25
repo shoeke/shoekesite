3449147bce3d44397a5658734e738e206e63ed2b
sabrina <hoekehouse@gmail.com>
2020-08-25 12:30:51 -0700

    mobile menu open

--- behave.js
+++ behave.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
 
     $(".hb-box").click(function(e) {
-        $(this).addClass("animation");
+        $(".hb-box").addClass("animation");
+        $(".modal-menu").slideDown("slow").css("display", "flex");
     });
+
+    $(".menu-closeup").click(function(e){
+        $(".modal-menu").slideUp("slow").css("display", "none");
+    });
+
+
   
   });
+/*
+  $(".hb-box").click(function(e) {
+    $(this).addClass("animation");
+})
+.click(function(e){
+    $(".modal-menu").show();
+});*/
