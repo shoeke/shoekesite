6847eeb71063a65336300b785e18a40b407dd5c7
sabrina <hoekehouse@gmail.com>
2020-08-27 11:43:36 -0700

    expanding portfolio on illo

--- behave.js
+++ behave.js
@@ -11,9 +11,23 @@ $(document).ready(function(){
         $(".dark-bg").fadeOut("slow");
         $(".modal-menu").slideUp("slow");
     });
+/*keep hidden section as display:grid in css and add "none" on page load to avoid slideDown defaulting to display: block*/
+$('.illo-row-remainder').css('display', 'none');
 
+
+$(".more-arrow").click(function(e){
+    $(".illo-row-remainder").slideToggle("slow");
+    $(".arrow-holder").toggleClass("flip");
+    if($(".more").is(':visible')) {
+        $(".more").fadeToggle("fast", function(){
+            $(".less").fadeToggle("slow");
+        });
+    }else{
+        $(".less").fadeToggle("fast", function(){
+            $(".more").fadeToggle("slow");
+        });
+    }
+});
     
 
   });
-
-  
